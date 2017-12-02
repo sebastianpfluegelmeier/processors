04c17b597c9fdab74f3e187cb7e95b8e2b467703
sebastian pfluegelmeier <pfluegelmeier.s@gmx.at>
2017-12-02 12:45:27 +0100

    Emergency commit

--- src/oscillators.rs
+++ src/oscillators.rs
@@ -8,7 +8,6 @@ pub struct Sine {
     phase: f32,
     samplerate: usize,
     table: Vec<f32>
-
 }
 
 impl Processor<[f32; 1]> for Sine {
@@ -46,6 +45,132 @@ impl Sine {
             phase: 0.0,
             samplerate: 1,
             table: table
+        }
+    }
+}
+
+pub struct Saw {
+    phase: f32,
+    samplerate: usize,
+}
+
+impl Processor<[f32; 1]> for Saw {
+
+    fn frame_process(&mut self, 
+                     inputs: &FrameSet<[f32; 1]>,
+                     outputs: &mut FrameSet<[f32; 1]>) {
+        outputs[0][0] = self.phase * 2.0 - 1.0;
+        self.phase = inputs[0][0] / self.samplerate as f32;
+        if self.phase > 1.0 {
+            self.phase -= 1.0;
+        }
+
+    }
+
+    fn set_samplerate(&mut self, samplerate: usize) {
+        self.samplerate = samplerate;
+    }
+
+    fn inputs_amt(&self) -> usize {
+        1
+    }
+
+    fn outputs_amt(&self) -> usize {
+        1
+    }
+}
+
+impl Saw {
+    pub fn new() -> Self {
+        Saw {
+            phase: 0.0,
+            samplerate: 1
+        }
+    }
+}
+
+pub struct Tri {
+    phase: f32,
+    samplerate: usize,
+}
+
+impl Processor<[f32; 1]> for Tri {
+
+    fn frame_process(&mut self, 
+                     inputs: &FrameSet<[f32; 1]>,
+                     outputs: &mut FrameSet<[f32; 1]>) {
+        outputs[0][0] = if self.phase < 0.5 
+                            { self.phase * 4.0 - 1.0 }
+                            else
+                            { self.phase * (-4.0) - 1.0 };
+        self.phase = inputs[0][0] / self.samplerate as f32;
+        if self.phase > 1.0 {
+            self.phase -= 1.0;
+        }
+
+    }
+
+    fn set_samplerate(&mut self, samplerate: usize) {
+        self.samplerate = samplerate;
+    }
+
+    fn inputs_amt(&self) -> usize {
+        1
+    }
+
+    fn outputs_amt(&self) -> usize {
+        1
+    }
+}
+
+impl Tri {
+    pub fn new() -> Self {
+        Tri {
+            phase: 0.0,
+            samplerate: 1
+        }
+    }
+}
+
+pub struct Square {
+    phase: f32,
+    samplerate: usize,
+}
+
+impl Processor<[f32; 1]> for Square {
+
+    fn frame_process(&mut self, 
+                     inputs: &FrameSet<[f32; 1]>,
+                     outputs: &mut FrameSet<[f32; 1]>) {
+        outputs[0][0] = if self.phase < 0.5 
+                            { 1.0 }
+                            else
+                            { -1.0 };
+        self.phase = inputs[0][0] / self.samplerate as f32;
+        if self.phase > 1.0 {
+            self.phase -= 1.0;
+        }
+
+    }
+
+    fn set_samplerate(&mut self, samplerate: usize) {
+        self.samplerate = samplerate;
+    }
+
+    fn inputs_amt(&self) -> usize {
+        1
+    }
+
+    fn outputs_amt(&self) -> usize {
+        1
+    }
+}
+
+impl Square {
+    pub fn new() -> Self {
+        Square {
+            phase: 0.0,
+            samplerate: 1
         }
     }
 }
